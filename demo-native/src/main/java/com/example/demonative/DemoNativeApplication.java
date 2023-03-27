3ffc433a48e4611cc45d460254d889ddccd34d35
jaspersteel <jaspersteelxx@gmail.com>
2023-03-27 08:48:43 +0800

    fix git issue

---
+++ demo-native/src/main/java/com/example/demonative/DemoNativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demonative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoNativeApplication.class, args);
+    }
+
+}
